8f24d7512c6c8fe38d1a756920a99bfe7bf48249
frezdev <carlos.andres.ff2912@gmail.com>
2024-02-25 22:57:01 -0500

    Update styles & add project info

---
+++ web/src/utils/projects.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: 'Tinder Swipe',
+    repo: 'https://github.com/frezdev/javascript-projects/tree/main/projects/01-tinder-swipe',
+    demo: '/projects/01-tinder-swipe/index.html',
+    image: '/projects/01-tinder-swipe/01.webp',
+    tags: ['JavaScript', 'HTML', 'CSS', 'DOM', 'Transitions', 'Events', 'Animations'],
+    description: 'Creating the tinder swipe effect',
+  },
+]
